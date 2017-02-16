bdef184d910249a0a16e81bf4abf1c1de0665a54
Ian Cottrell <iancottrell@google.com>
2017-02-16 11:33:20 +0000

    Building with bazel

--- core/data/pod/doc.go
+++ core/data/pod/doc.go
@@ -14,3 +14,9 @@
 
 // Package pod provides types and functions for dealing with simple structured objects.
 package pod
+
+// The following are the imports that generated source files pull in when present
+// Having these here helps out tools that can't cope with missing dependancies
+import (
+	_ "github.com/golang/protobuf/proto"
+)
